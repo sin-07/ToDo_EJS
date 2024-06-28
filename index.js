52f8cd93fb9f58094e5039676fa36fc16a25863d
Aniket singh <aniket.singh9322@gmail.com>
2024-06-29 00:00:42 +0530

    projectStart

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const express = require("express");
 const app = express();
 const path = require('path');
 
+app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
 
 app.get("/",function(req,res){
     res.render("index")
 })
-app.get("/itsme/:username",function(req,res){
-    res.send(`Hey this side ${req.params.username}`)
-})
+// app.get("/itsme/:username",function(req,res){
+//     res.send(`Hey this side ${req.params.username}`)
+// })
 
-app.listen(3000, function(){
-    console.log("Server is running on port 3000");
-});
+// app.listen(3000, function(){
+//     console.log("Server is running on port 3000");
+// });
+
+app.listen(3000)
